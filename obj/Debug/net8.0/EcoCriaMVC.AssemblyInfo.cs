850fbef6b21f2f13b1662af473648b72eae18c44
Diego Matheus <dms.diego.2019@gmail.com>
2024-10-26 20:59:54 -0300

    botao alterado do ponto

--- obj/Debug/net8.0/EcoCriaMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcoCriaMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoCriaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2579f20c92a106649ae9a10e506fc1ae78ceba3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556050b433e9b7d7ff25fe994b8f0a715e8170a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoCriaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoCriaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
