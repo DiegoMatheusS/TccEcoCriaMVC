b05024f7a9785bd59071b0e74e764006a299cd7b
Diego Matheus <dms.diego.2019@gmail.com>
2024-11-18 20:46:40 -0300

    atualizado login-dropdown

--- obj/Debug/net8.0/EcoCriaMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcoCriaMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoCriaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+850fbef6b21f2f13b1662af473648b72eae18c44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b890f425ae6232b287fcf55fb6d139abde92ba3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoCriaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoCriaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
